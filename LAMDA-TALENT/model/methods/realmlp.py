72ed221c8720d987f212dd46cb2d4fc3d53ddfe2
211250116-Cai Haorun <211250116@smail.nju.edu.cn>
2024-07-30 23:52:48 +0800

    Allow RealMLP to use the validation set for validation

--- LAMDA-TALENT/model/methods/realmlp.py
+++ LAMDA-TALENT/model/methods/realmlp.py
@@ -83,7 +83,6 @@ class RealMLPMethod(Method):
         self.is_binclass, self.is_multiclass, self.is_regression = self.D.is_binclass, self.D.is_multiclass, self.D.is_regression
         self.n_num_features, self.n_cat_features = self.D.n_num_features, self.D.n_cat_features
         
-        
         if config:
             self.reset_stats_withconfig(config)
         self.data_format(is_train = True)
@@ -106,9 +105,14 @@ class RealMLPMethod(Method):
             cat_features = [1] * self.C['train'].shape[1] + [0] * self.N['train'].shape[1]
         
         y_train = np.array(self.y['train'])
+        y_val = np.array(self.y['val'])
+        
+        X_train_and_val = np.concatenate([X_train, X_val], axis=0)
+        y_train_and_val = np.concatenate([y_train, y_val], axis=0)
+        val_idxs = np.arange(X_train.shape[0], X_train.shape[0] + X_val.shape[0], dtype=np.int32)
 
         tic = time.time()
-        self.model.fit(X_train, y_train, cat_features=cat_features)
+        self.model.fit(X_train_and_val, y_train_and_val, val_idxs=val_idxs, cat_features=cat_features)
         
         test_logit = self.model.predict(X_val)
         test_label = self.y['val']
